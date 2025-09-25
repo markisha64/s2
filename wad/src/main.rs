1f73e86ee2d89585b3f9916ebecb267be6185ef5
Marko Grizelj <marko.grizelj@gauss.dev>
2025-09-25 11:23:10 +0200

    feat: actual commands

--- wad/src/main.rs
+++ wad/src/main.rs
@@ -1,18 +1,53 @@
-use std::path::PathBuf;
+use std::{fs::File, path::PathBuf};
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use wad::{parse_wad, rebuild_wad};
 
 #[derive(Parser, Debug)]
 struct Args {
-    wad_file: PathBuf,
-    output: PathBuf,
+    #[command(subcommand)]
+    command: Command,
 }
 
-fn main() {
+#[derive(Subcommand, Debug)]
+enum Command {
+    Unpack {
+        wad_file: PathBuf,
+        output_dir: PathBuf,
+        json_out: PathBuf,
+    },
+    Pack {
+        json_in: PathBuf,
+        output_file: PathBuf,
+    },
+}
+
+fn main() -> anyhow::Result<()> {
     let args = Args::parse();
 
-    let manifest = parse_wad(args.wad_file, args.output).unwrap();
+    match args.command {
+        Command::Unpack {
+            wad_file,
+            output_dir,
+            json_out,
+        } => {
+            let manifest = parse_wad(wad_file, output_dir)?;
 
-    rebuild_wad(manifest, PathBuf::from("./test.wad")).unwrap();
+            let json_out = File::create(json_out)?;
+
+            serde_json::to_writer_pretty(json_out, &manifest)?;
+        }
+        Command::Pack {
+            json_in,
+            output_file,
+        } => {
+            let json_in = File::open(json_in)?;
+
+            let manifest = serde_json::from_reader(json_in)?;
+
+            rebuild_wad(manifest, output_file)?;
+        }
+    }
+
+    Ok(())
 }
