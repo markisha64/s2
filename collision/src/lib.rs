bb7bc86c34268e13d077aa817fe7a28e5ca7b329
markisha64 <marko.grizelj03@gmail.com>
2025-10-06 01:02:42 +0200

    feat: more parsing

--- collision/src/lib.rs
+++ collision/src/lib.rs
@@ -6,9 +6,20 @@ use std::{
 
 use serde::{Deserialize, Serialize};
 
-pub struct CollisionHeader {
-    pub offset: u32,
-    pub unk_len: u32,
+// section 0
+pub struct ColissionHeader {
+    pub section_1_offset: u32,
+    pub section_0_data_len: u32,
+}
+
+pub struct ColissionSection1 {
+    pub section_3_offset: u32,
+    pub section_2_offset: u32,
+}
+
+pub struct ColissionSection2 {
+    pub section_2_data_len: u32,
+    pub section_4_offset: u32,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -41,25 +52,73 @@ pub fn parse_colission(
 
     let mut buffer_0 = [0u8; 4];
     let mut buffer_1 = [0u8; 4];
+
     file.read_exact(&mut buffer_0)?;
     file.read_exact(&mut buffer_1)?;
 
-    let offset = u32::from_le_bytes(buffer_0);
-    let unk_len = u32::from_le_bytes(buffer_1);
+    let header = ColissionHeader {
+        section_1_offset: u32::from_le_bytes(buffer_0),
+        section_0_data_len: u32::from_le_bytes(buffer_1),
+    };
 
-    let mut file_0 = output_dir.clone();
-    file_0.push("file_0.bin");
+    let mut section_0_pb = output_dir.clone();
+    section_0_pb.push("section_0.dat");
 
-    let mut dst_file = File::create(&file_0)?;
+    let mut section_0_file = File::create(section_0_pb)?;
 
-    copy_file(&mut file, &mut dst_file, offset as u64 - 8)?;
+    copy_file(
+        &mut file,
+        &mut section_0_file,
+        header.section_1_offset as u64 - 8,
+    )?;
 
-    let mut file_1 = output_dir.clone();
-    file_1.push("file_1.bin");
+    file.read_exact(&mut buffer_0)?;
+    file.read_exact(&mut buffer_1)?;
 
-    let mut dst_file = File::create(&file_1)?;
+    let section_1 = ColissionSection1 {
+        section_3_offset: u32::from_le_bytes(buffer_0),
+        section_2_offset: u32::from_le_bytes(buffer_1),
+    };
 
-    copy_file(&mut file, &mut dst_file, 999999)?;
+    let section_1_data_len = (section_1.section_2_offset - 4) / 28;
+
+    let mut section_1_pb = output_dir.clone();
+    section_1_pb.push("section_1.dat");
+
+    let mut section_1_file = File::create(section_1_pb)?;
+
+    copy_file(
+        &mut file,
+        &mut section_1_file,
+        section_1_data_len as u64 * 28,
+    )?;
+
+    file.read_exact(&mut buffer_0)?;
+    file.read_exact(&mut buffer_1)?;
+
+    let section_2 = ColissionSection2 {
+        section_2_data_len: u32::from_le_bytes(buffer_0),
+        section_4_offset: u32::from_le_bytes(buffer_1),
+    };
+
+    let mut section_2_offsets_pb = output_dir.clone();
+    section_2_offsets_pb.push("section_2_offsets.dat");
+
+    let mut section_2_offsets_file = File::create(section_2_offsets_pb)?;
+
+    copy_file(
+        &mut file,
+        &mut section_2_offsets_file,
+        section_2.section_2_data_len as u64 * 4,
+    )?;
+
+    let mut tail = output_dir.clone();
+    tail.push("tail.bin");
+
+    let mut tail_file = File::create(tail)?;
+
+    // copy to end
+    copy_file(&mut file, &mut tail_file, 999999999)?;
 
     Ok(CollisionManifest {})
 }
